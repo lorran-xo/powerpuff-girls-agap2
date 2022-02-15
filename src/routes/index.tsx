f4d636c37a8ec7e4306b98786591b0a80af99f0c
Lorran Oliveira <lorranxjf@gmail.com>
2022-02-15 20:34:01 -0300

    feat: implement react context for data-flow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Details from '../pages/Details';
+import { MyProvider } from '../Context';
 
 export default function MainRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/details" element={<Details />} />
-    </Routes>
+    <MyProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
+    </MyProvider>
   );
 }
